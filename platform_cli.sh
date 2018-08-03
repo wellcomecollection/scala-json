5ebc672f7b161c8557e0aeb4fd8690b992d9a480
Alex Chan <a.chan@wellcome.ac.uk>
2018-08-03 12:00:40 +0100

    Just on pull requests, load a specific commit

--- platform_cli.sh
+++ platform_cli.sh
@@ -3,9 +3,11 @@
 set -o errexit
 set -o nounset
 
+COMMIT="b5604da0fc49ba9369b409f75c4e0eb755c423b9"
+
 if [ ! -f platform_cli.py ]
 then
-  curl "https://raw.githubusercontent.com/wellcometrust/platform/master/builds/platform_cli.py" > platform_cli.py
+  curl "https://raw.githubusercontent.com/wellcometrust/platform/$COMMIT/builds/platform_cli.py" > platform_cli.py
 end
 
 python platform_cli.py "$@"
